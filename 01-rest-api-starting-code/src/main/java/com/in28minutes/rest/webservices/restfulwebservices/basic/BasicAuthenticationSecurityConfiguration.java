55496f1794eead25708ce82c6f5fcd8d1fc9fdc0
Mahmoudelsayed5 <mahmoud.elsayed2@aiesec.net>
2024-03-29 14:38:39 +0200

    second commit

--- 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
+++ 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices.basic;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,7 +25,7 @@ public class BasicAuthenticationSecurityConfiguration {
 				.authorizeHttpRequests(
 					auth -> 
 						auth
-						.antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+						.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 						.anyRequest().authenticated()
 					)
 				.httpBasic(Customizer.withDefaults())
---
+++ 01-rest-api-starting-code/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo (id,description, done, target_date, username)
+values(10001, 'learn aws', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id,description, done, target_date, username)
+values(10002, 'Get aws certified', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id,description, done, target_date, username)
+values(10003, 'Learn devops', false, CURRENT_DATE(), 'in28minutes');
